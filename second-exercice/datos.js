646147742892bd0985cd01bdfd902f160eee755f
oscarjulianguavita <oguavita@gmail.com>
2023-06-01 20:52:01 +0200

    implementos los ejercicios

---
+++ second-exercice/datos.js
@@ -0,0 +1,12 @@
+const miName = "Oscar Julian Guavita Rojas";
+let age = 40;
+let isProgrammer = true ;
+const date = "19-01-1983";
+const book = {
+    title:"el principito",
+    author: "Antoine de Saint-Exupéry",
+    date: "6 de abril de 1943",
+    url: "https://es.wikipedia.org/wiki/El_principito"
+}
+const miarray = [miName,age,isProgrammer,date, book];
+console.log(miarray);
