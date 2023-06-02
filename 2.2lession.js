5bc6870586ba12d0e6d8bb300822602b9cbc362b
oscarjulianguavita <oguavita@gmail.com>
2023-06-02 10:37:13 +0200

    tema 2

---
+++ 2.2lession.js
@@ -0,0 +1,15 @@
+////variables y constantes
+
+//sumar
+function sumar (){
+    return 56+5;
+}
+console.log(sumar ());
+
+
+//type of
+var num = 4
+console.log(typeof num);
+
+num = "Hola soy un num"
+console.log(typeof num);
---
+++ second-exercice/2.4listas.js
@@ -0,0 +1,4 @@
+//lista  ó array(arreglos), conjunto de variables puestas en orden
+const lista = [1,"hola",true,undefined,null]
+
+console.log(lista);
