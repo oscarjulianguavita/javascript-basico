217440bb2edc114182a286b02c8321ae0538a26b
oscarjulianguavita <oguavita@gmail.com>
2023-06-07 13:12:01 +0200

    temario tres

---
+++ third-exercise/3.1.1switch.js
@@ -0,0 +1,27 @@
+//sentencias switch
+
+
+let nota =5;
+switch (nota){
+    case 5:
+        console.log("Enhorabuena has obtenido un sobresaliente");
+        break;
+    case 4:
+        console.log("Enhorabuena has obtenido un sobresaliente");
+        break;
+    case 3:
+        console.log("Enhorabuena has obtenido un sobresaliente");
+        break;
+    case 2:
+        console.log("Enhorabuena has obtenido un sobresaliente");
+        break;
+    case 1:
+        console.log("Enhorabuena has obtenido un sobresaliente");
+        break;
+        default:
+            console.log("error"); 
+            break;
+
+            
+
+}
---
+++ third-exercise/3.1if-else.js
@@ -0,0 +1,34 @@
+//bifurcaciones permite definir, si ir por un camino o por otro.
+//if. else. switch
+
+let saldo = 150;
+let efectivo =100;
+
+
+if (efectivo < saldo){
+    console.log("puedes sacar dinero");
+}
+if (efectivo < saldo){
+    console.log("puedes sacar dinero");
+} else {
+    console.log("saldo insuficiente");
+}
+//if else +  if else.
+
+let calificacion = 10 ;
+if (nota === 5){
+    console.log("Enhorabuena has obtenido un sobresaliente");
+} else if (calificacion === 4){
+    console.log("Buen trabajo pero puedes hacerlo mejor");
+}else if (calificacion === 3){
+    console.log("Has obtenidoun suficiente");
+}else if (calificacion === 2){
+    console.log("No has aprobado por poco");
+}else if (calificacion === 1){
+    console.log("No has estudiado nada trabaja mas para la proxima");
+}else{
+    console.log("error, introduce una nota entre el 1 y el 5");
+};
+
+
+
